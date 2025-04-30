cae030fbd38445b59b9d9fb31ca7df6cba8df43d
Tatang Bukhori <tatangb909@gmail.com>
2025-04-30 22:24:47 +0700

    Repair Food Table

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Food extends Model
 {
     use HasFactory;
-    protected $table = 'foods';
 
     protected $guarded = [];
 }
--- database/migrations/2025_04_30_151006_create_food_table.php
+++ database/migrations/2025_04_30_151006_create_food_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('foods', function (Blueprint $table) {
+        Schema::create('food', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('slug');
             $table->string('price');
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('foods');
+        Schema::dropIfExists('food');
     }
 };
